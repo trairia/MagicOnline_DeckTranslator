3fb5e0d6cf79be9370d9d7509a7f49313bbf25da
Kazunori Kimura <empty_thinking@hotmail.com>
2012-03-11 23:08:35 +0900

    yaml data to datastore @ upload.py upload.html : template of upload script/DKA.htm sample html

--- MODeckTrans/upload.py
+++ MODeckTrans/upload.py
@@ -2,19 +2,27 @@
 # -*- coding:utf-8 -*-
 import os
 import cgi
+import yaml
 
 from google.appengine.api import users
 from google.appengine.ext import webapp, db
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+class MtGCard(db.Model):
+    name = db.TextProperty(required=True)
+    name_ja = db.TextProperty()
+    cost = db.StringProperty()
+    MainType = db.IntegerProperty()
+    SubType = db.ListProperty(str)
+
 class MainPage(webapp.RequestHandler):
     def get(self):
         pass
 
-class upload(webapp.RequestHandler):
+class upload_decklist(webapp.RequestHandler):
     def get(self):
-        template_values = {}
+        template_values = {'action':'/upload_decklist'}
         path = os.path.join(os.path.dirname(__file__), 'upload.html')
         self.response.out.write(template.render(path, template_values))
         
@@ -27,14 +35,41 @@ class upload(webapp.RequestHandler):
         print main_deck
         print sideboard
 
-        # for line in upload_file:
-        #     self.response.out.write(line)
-        #     self.response.out.write('<br>')
-#        self.response.out.write(upload_file)
+class upload_cardlist(webapp.RequestHandler):
+    def get(self):
+        template_values = {'action':'/upload_cardlist'}
+        path = os.path.join(os.path.dirname(__file__), 'upload.html')
+        self.response.out.write(template.render(path, template_values))
 
+    def post(self):
+        upload_file = self.request.get('fileName')
+        try:
+            cardlist = yaml.load(upload_file)
+            for card in cardlist:
+                if card['SubType'][0] == None:
+                    card['SubType'][0] = ''
+                
+                if str(card['Cost']):
+                    card['Cost'] = str(card['Cost'])
+
+                obj = MtGCard(name = card['Name_en'],
+                              name_ja = card['Name_ja'],
+                              cost = card['Cost'],
+                              MainType = card['MainType'],
+                              SubType = card['SubType'])
+                obj.put()
+            self.response.out.write("%d card added" % len(cardlist))
+            self.response.out.write("<br>")
+            
+        except yaml.YAMLError, exc:
+            if hasattr(exc, 'probem_mark'):
+                mark = exc.problem_mark
+                print "Error position: (%s:%s)"%(mark.line+1, mark.column+1)
+                                                
 application = webapp.WSGIApplication(
     [('/',MainPage),
-     ('/upload',upload)],
+     ('/upload_decklist',upload_decklist),
+     ('/upload_cardlist',upload_cardlist)],
     debug=True)
 
 def main():
