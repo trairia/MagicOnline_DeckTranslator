6243a17c3dd030f2e1c67035ad4c245f2ce6ef7c
Kazunori Kimura <empty_thinking@hotmail.com>
2012-03-15 20:10:16 +0900

    create bulkloader configuration

---
+++ bulkloader/dbfuncs.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from google.appengine.ext import db
+
+class MtGCard(db.Model):
+    name = db.StringProperty(required=True)
+    name_ja = db.StringProperty()
+    cost = db.StringProperty()
+    MainType = db.IntegerProperty()
+    SubType = db.ListProperty(str)
+
+class ChangeLogModel(db.Model):
+    message = db.TextProperty()
+    date    = db.DateTimeProperty(auto_now_add=True)
+
+def getCardFromDS(cardname):
+    query = db.Query(MtGCard)
+    query.filter('name = ', cardname)
+    result = query.get()
+    card = {}
+    if result:
+        card['Name_en']  = cardname
+        card['Name_ja']  = result.name_ja
+        card['cost']     = result.cost
+        card['mainType'] = result.MainType
+        card['subType']  = result.SubType
+    else:
+        card['Name_en']  = cardname
+        card['Name_ja']  = cardname
+        card['cost']     = None
+        card['mainType'] = 0
+        card['subType']  = None
+
+    return card
+
+def new_or_overwrite(card):
+    query = db.Query(MtGCard)
+    query.filter('name = ', card['Name_en'])
+    result = query.get()
+    if result:
+        result.name_ja  = card['Name_ja']
+        result.cost     = card['Cost']
+        result.MainType = card['MainType']
+        result.SubType  = card['SubType']
+        result.put()
+    else:
+        obj = MtGCard(name = card['Name_en'],
+                      name_ja = card['Name_ja'],
+                      cost = card['Cost'],
+                      MainType = card['MainType'],
+                      SubType = card['SubType'])
+        obj.put()
+
+    
+        
---
+++ bulkloader/loader.py
@@ -0,0 +1,33 @@
+#!usr/bin/env python
+
+from google.appengine.tools import bulkloader
+from dbfuncs import MtGCard
+import yaml
+
+class DataLoader(bulkloader.Loader):
+    def __init__(self):
+        bulkloader.Loader.__init__(self, 'MtGCard',
+                                   [('name', unicode),
+                                    ('name_ja', unicode),
+                                    ('cost',str),
+                                    ('MainType',int),
+                                    ('SubType',list)])
+
+    def generate_key(self, i, values):
+        return values[0]
+
+    def generate_records(self, filename):
+        file = open(filename)
+        def generator():
+            for card in yaml.load(file.read().decode('utf-8')):
+                subtype = ['None'] if card['SubType'][0]==None else card['SubType']
+                yield [card['Name_en'],
+                       card['Name_ja'],
+                       card['Cost'],
+                       card['MainType'],
+                       subtype
+                    ]
+            
+        return generator()
+
+loaders = [DataLoader]
