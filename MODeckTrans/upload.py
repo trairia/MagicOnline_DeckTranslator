83d1b6e4bf44837e84215c6220be68061614a46b
Kazunori Kimura <empty_thinking@hotmail.com>
2012-03-19 21:39:25 +0900

    デッキの出力処理を関数に分離。 txtファイルの他、テキストフォームからの入力を追加

--- MODeckTrans/upload.py
+++ MODeckTrans/upload.py
@@ -28,34 +28,47 @@ class upload_decklist(webapp.RequestHandler):
         self.response.out.write(template.render(path, template_values))
         
     def post(self):
-        deck = cardtype.MtGDeck()
+
         path = os.path.join(os.path.dirname(__file__), 'htdocs/result.html')
         self.request.charset = 'utf-8'
         upload_file = self.request.get('fileName').decode('utf-8')
+        textdeck = self.request.get('textdeck').decode('utf-8')
+
         if upload_file:
-            now = datetime.now()
-            deck.fromString(upload_file)
+            self.renderDeck(upload_file)
 
-            num_lands = sum([l[1] for l in deck.MainDeck['Land']])
-            num_creatures = sum([l[1] for l in deck.MainDeck['Creature']])
-            num_spells = sum([l[1] for l in deck.MainDeck['Spells']])
-            num_sideboard = sum(l[1] for l in deck.SideBoard)
-
-            template_values = {'Lands' : deck.MainDeck['Land'],
-                               'num_lands' : num_lands,
-                               'Creatures' : deck.MainDeck['Creature'],
-                               'num_creatures' : num_creatures,
-                               'Spells' : deck.MainDeck['Spells'],
-                               'num_spells' : num_spells,
-                               'num_sideboard' : num_sideboard,
-                               'sideboard' : deck.SideBoard}
-
-            self.response.out.write(template.render(path, template_values))
+        elif textdeck:
+            self.renderDeck(textdeck)
 
         else:
             template_values = {'action':'/upload_decklist'}
             path = os.path.join(os.path.dirname(__file__), 'htdocs/index.html')
             self.response.out.write(template.render(path, template_values))
+
+    def renderDeck(self, deck):
+        deckdata = cardtype.MtGDeck()
+        path = os.path.join(os.path.dirname(__file__), 'htdocs/result.html')
+        deckdata.fromString(deck)
+        num_lands = sum([l[1] for l in deckdata.MainDeck['Land']])
+        num_creatures = sum([l[1] for l in deckdata.MainDeck['Creature']])
+        num_spells = sum([l[1] for l in deckdata.MainDeck['Spells']])
+        if (len(deckdata.SideBoard)):
+            num_sideboard = 0
+        else:
+            num_sideboard = sum(l[1] for l in deckdata.SideBoard)
+
+        template_values = {'Lands' : deckdata.MainDeck['Land'],
+                           'num_lands' : num_lands,
+                           'Creatures' : deckdata.MainDeck['Creature'],
+                           'num_creatures' : num_creatures,
+                           'Spells' : deckdata.MainDeck['Spells'],
+                           'num_spells' : num_spells,
+                           'num_sideboard' : num_sideboard,
+                           'sideboard' : deckdata.SideBoard}
+
+        self.response.out.write(template.render(path, template_values))
+
+
 
 class Whatis(webapp.RequestHandler):
     def get(self):
